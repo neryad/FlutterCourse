6a287105f0227594a464db7dac87f7c13c2b06e3
neryad <dayern.gomez@gmail.com>
2019-06-04 16:49:25 -0400

    avatar

--- componentes/lib/src/pages/alert_page.dart
+++ componentes/lib/src/pages/alert_page.dart
@@ -9,12 +9,52 @@ class AlertPAge extends StatelessWidget {
      appBar: AppBar(
        title: Text('Alert Page'),
      ),
+     body: Center(
+       child: RaisedButton(
+         child: Text('Mostrar Alerta'),
+         color: Colors.blue,
+         textColor: Colors.white,
+         shape: StadiumBorder(),
+         onPressed: ()=>_mostrarAlert(context),
+       ),
+     ),
      floatingActionButton: FloatingActionButton(
        child: Icon(Icons.ac_unit),
        onPressed: (){
          Navigator.pop(context);
        },
        ),
+    );
+  }
+
+  void _mostrarAlert(BuildContext context){
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context){
+        return AlertDialog(
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Este es el contenido de la caja de dialogo'),
+              FlutterLogo(size: 100.0)
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancelar'),
+              onPressed: ()=> Navigator.of(context).pop(),
+            ),
+             FlatButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      }
     );
   }
 }
--- componentes/lib/src/pages/avatar_page.dart
+++ componentes/lib/src/pages/avatar_page.dart
@@ -8,6 +8,30 @@ class AvatarPAge extends StatelessWidget {
     return Scaffold(
      appBar: AppBar(
        title: Text('Avatar Page'),
+       actions: <Widget>[
+         Container(
+           padding: EdgeInsets.all(2.0),
+           child: CircleAvatar(
+             backgroundImage: NetworkImage('https://dam.smashmexico.com.mx/wp-content/uploads/2018/09/26165403/spidermantop10_portada.jpg'),
+             radius: 25.0,
+           ),
+         ),
+         Container(
+           margin: EdgeInsets.only(right: 10.0),
+           child: CircleAvatar(
+             child: Text('SL'),
+             backgroundColor: Colors.redAccent,
+             foregroundColor: Colors.white,
+           ),
+         )
+       ],
+     ),
+     body: Center(
+       child: FadeInImage(
+         image: NetworkImage('https://img.game.co.uk/ml2/5/6/2/9/562944_scr9_a.png'),
+         placeholder: AssetImage('assets/jar-loading.gif'),
+         fadeInDuration: Duration( microseconds: 200),
+         ),
      ),
     );
   }
