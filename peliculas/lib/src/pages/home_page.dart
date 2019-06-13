44230c973889fc35b032aaa70c16e0190ea03b7c
neryad <dayern.gomez@gmail.com>
2019-06-13 17:57:03 -0400

    video 101

---
+++ peliculas/lib/src/pages/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/provider/peliculas_provider.dart';
+
+import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+class HomePage extends StatelessWidget {
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Peliculas en cines'),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon (Icons.search),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _swiperTajertas()
+          ],),
+      )
+    );
+  }
+
+  Widget _swiperTajertas(){
+
+    final peliculasProvider = new PeliculasProvider();
+    peliculasProvider.getEnCines();
+
+
+    return  CardSwiper(
+      peliculas: [1,2,3,4,5],
+    );
+  }
+}
